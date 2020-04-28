3cbc0081ec05be0857b152d82c843662616b2e8a
Gavin Love <glove@Gavins-MacBook-Pro.local>
2020-04-28 11:18:56 -0500

    Refactor mirage

--- config/environment.js
+++ config/environment.js
@@ -30,8 +30,8 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV['ember-cli-mirage'] = {
-      enabled: false,
-      autostart: false
+      enabled: true,
+      autostart: true
     };
   }
 
